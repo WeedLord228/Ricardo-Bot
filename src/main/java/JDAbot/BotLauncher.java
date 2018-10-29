d0eed367aee0815da274ec53c730ae4235d4eb70
WeedLord228 <zajtzevar@gmail.com>
2018-10-29 13:15:39 +0500

    Rewriting gradle for maven without functions

---
+++ src/main/java/JDAbot/BotLauncher.java
@@ -0,0 +1,15 @@
+package JDAbot;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class BotLauncher {
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = new JDABuilder("NDkzNzA3NDMzMjk5MTQ4ODAw.Do4pGw.eZl9Iu0_GqzJS3kDvNY4KAqzXeU")
+                .build();
+    }
+
+}
