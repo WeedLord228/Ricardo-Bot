d1f8a924d2dbe4c1467840d31db2065530de648e
WeedLord228 <zajtzevar@gmail.com>
2018-11-08 11:37:59 +0500

    Revert "Good night functional"
    This reverts commit 86c4fe9d105304261b1cfa4fa5204273f53a6bb8.

--- src/main/java/JDAbot/MyListener.java
+++ src/main/java/JDAbot/MyListener.java
@@ -9,19 +9,18 @@ public class MyListener extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().startsWith("!"))
+        //Мы хотим чтобы бот обрабатывал только команды
+        {
+            String[] args = event.getMessage().getContentRaw().split(" ");
+            //Разделение входящего сообщения на массив аргументов,
+            //предпологается что боту из входящих будут нужны только команды и их аргументы
 
-//        if (event.getMessage().getContentRaw().startsWith("!"))
-        //Мы не хотим чтобы бот обрабатывал только команды
+            MessageChannel channel = event.getChannel();
+            //Канал, из которго пришло сообщение
 
-        String[] args = event.getMessage().getContentRaw().split(" ");
-        //Разделение входящего сообщения на массив аргументов,
-        //предпологается что боту из входящих будут нужны только команды и их аргументы
-
-        MessageChannel channel = event.getChannel();
-        //Канал, из которго пришло сообщение
-
-        channel.sendMessage(ChatOutputCommands.executeCommand(args)).queue();
-        //Распознование и выполнение команды
-
+            channel.sendMessage(ChatOutputCommands.executeCommand(args)).queue();
+            //Распознование и выполнение команды
+        }
     }
 }
