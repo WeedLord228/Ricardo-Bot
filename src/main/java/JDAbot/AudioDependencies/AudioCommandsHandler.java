98c16884821fb8eb62c05f82343fbc64cc2fa4ac
WeedLord228 <zajtzevar@gmail.com>
2018-11-25 22:03:33 +0500

    Optimizing Mylistener and adding AudioCommandsHandler
    Функции распознавания и выполнения команды были вынесены в отдельный класс, а так же была дополнена сущность дискорд плеера

---
+++ src/main/java/JDAbot/AudioDependencies/AudioCommandsHandler.java
@@ -0,0 +1,84 @@
+package JDAbot.AudioDependencies;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+
+public class AudioCommandsHandler {
+
+    DiscordAudioPlayer discordAudioPlayer;
+
+    public AudioCommandsHandler(DiscordAudioPlayer discordAudioPlayer)
+    {
+        this.discordAudioPlayer = discordAudioPlayer;
+    }
+
+    public void executeCommand (String[] args,VoiceChannel voiceChannel,MessageChannel messageChannel){
+        if (args[0].equals("$play"))
+            play(args,voiceChannel,messageChannel);
+        if (args[0].equals("$disconnect"))
+            disconnect(args);
+        if (args[0].equals("skip"))
+            skip(args);
+    }
+
+    private void loadAndQueue (String[] args, final MessageChannel messageChannel)
+    {
+        String id = getId(args,messageChannel);
+
+        discordAudioPlayer.playerManager.loadItem(id, new AudioLoadResultHandler() {
+
+            public void trackLoaded(AudioTrack audioTrack)
+            {
+                discordAudioPlayer.scheduler.queue(audioTrack);
+                messageChannel.sendMessage("Ваш трек в очереди на " +
+                        (discordAudioPlayer.scheduler.queue.size() + 1)
+                        + "-м месте.").queue();
+            }
+
+            public void playlistLoaded(AudioPlaylist audioPlaylist) {
+
+            }
+
+            public void noMatches() {
+                messageChannel.sendMessage("Трек не найден").queue();
+                return;
+            }
+
+            public void loadFailed(FriendlyException e) {
+                messageChannel.sendMessage("Не удалось загрузить").queue();
+                return;
+            }
+        });
+    }
+
+    private String getId(String[] args,MessageChannel messageChannel)
+    {
+        if (args[1].length() < 11)
+        {
+            messageChannel.sendMessage("Неправильный track identifier, попробуйте обратиться к комнде !help").queue();
+            return null;
+        }
+        else return args[1].split("v=")[1].substring(0, 11);
+    }
+
+    private void play(String[] args, VoiceChannel voiceChannel,MessageChannel messageChannel)
+    {
+        discordAudioPlayer.openAudioConnection(voiceChannel);
+        loadAndQueue(args,messageChannel);
+    }
+
+    private void disconnect(String[] args)
+    {
+        discordAudioPlayer.audioManager.closeAudioConnection();
+    }
+
+    private void skip(String[] args)
+    {
+        discordAudioPlayer.scheduler.nextTrack();
+    }
+}
+
--- src/main/java/JDAbot/Main/MyListener.java
+++ src/main/java/JDAbot/Main/MyListener.java
@@ -1,17 +1,18 @@
 package JDAbot.Main;
 
 
-import JDAbot.AudioDependencies.BotPlayer;
+import JDAbot.AudioDependencies.AudioCommandsHandler;
+import JDAbot.AudioDependencies.DiscordAudioPlayer;
 import JDAbot.AudioDependencies.MySendHandler;
 import JDAbot.Logic.StringOutputCommands;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import net.dv8tion.jda.core.entities.Guild;
-import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.VoiceChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -20,15 +21,15 @@ import net.dv8tion.jda.core.managers.AudioManager;
 
 
 public class MyListener extends ListenerAdapter {
-    //Нужно создать плеер заранне, чтобы при каждом событии он не сбрасывался
-    private BotPlayer botPlayer = new BotPlayer();
+
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot())
             return;
 
-        if (event.getMessage().getContentRaw().startsWith("!"))
+        if (event.getMessage().getContentRaw().startsWith("!") ||
+                event.getMessage().getContentRaw().startsWith("$"))
         //Мы хотим чтобы бот обрабатывал только команды
         {
 
@@ -36,88 +37,33 @@ public class MyListener extends ListenerAdapter {
             //Разделение входящего сообщения на массив аргументов,
             //предпологается что боту из входящих будут нужны только команды и их аргументы
 
-            MessageChannel textChannel = event.getChannel();
-            //Канал, из которго пришло сообщение
+            Guild guild = event.getGuild();
+//            Будет необходима для создания аудио соединения
 
-            textChannel.sendMessage(StringOutputCommands.executeCommand(args)).queue();
-            //Распознование и выполнение команды
-
-            textChannel.sendMessage(Message)
-        }
-
-        if (event.getMessage().getContentRaw().startsWith("$")) {
-            String[] args = event.getMessage().getContentRaw().split(" ");
+            VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel();
+            //Аудио канал, в котором сидит пользователь в данный момент, нужно чтобы подкючить бота в его канал
 
             final MessageChannel messageChannel = event.getChannel();
+            //Канал, из которго пришло сообщение
 
-            Guild guild = event.getGuild();
-            AudioManager manager = guild.getAudioManager();
-            manager.setSendingHandler(new MySendHandler(botPlayer.player));
-
-            if (args[0].equals("$play")) {
-                if (args[1].length() < 11)
-                {
-                    messageChannel.sendMessage("Неправильный track identifier, попробуйте обратиться к комнде !help").queue();
-                    return;
-                }
-
-                String youTubeId = args[1].split("v=")[1].substring(0, 11);
-
-                VoiceChannel voiceChannel = event.getMember().getVoiceState().getChannel();
-                //Получаем голосовой канал пользователя
-
-//                final AudioPlayerManager playerManager = botPlayer.playerManager;
-                AudioSourceManagers.registerRemoteSources(botPlayer.playerManager);
-
-//                final AudioPlayer player =  botPlayer.player;
-
-                YoutubeAudioSourceManager youtubeASM = new YoutubeAudioSourceManager();
-
-                manager.setSendingHandler(new MySendHandler(botPlayer.player));
-                manager.openAudioConnection(voiceChannel);
-                //Подключение бота в голосовой канал
-
-                botPlayer.playerManager.registerSourceManager(youtubeASM);
-                //регистрация внешних источников треков
-
-                botPlayer.playerManager.loadItem(youTubeId, new AudioLoadResultHandler() {
-
-                    public void trackLoaded(AudioTrack audioTrack)
-                    {
-                        botPlayer.scheduler.queue(audioTrack);
-                        messageChannel.sendMessage("Ваш трек в очереди на " +
-                                (botPlayer.scheduler.queue.size() + 1)
-                                + "-м месте.").queue();
-                    }
-
-                    public void playlistLoaded(AudioPlaylist audioPlaylist) {
-
-                    }
-
-                    public void noMatches() {
-                        messageChannel.sendMessage("Трек не найден").queue();
-                        return;
-                    }
-
-                    public void loadFailed(FriendlyException e) {
-                        messageChannel.sendMessage("Не удалось загрузить").queue();
-                        return;
-                    }
-                });
-
-
-            }
-
-            if (args[0].equals("$skip"))
+            if (event.getMessage().getContentRaw().startsWith("$"))
             {
-                botPlayer.scheduler.nextTrack();
-            }
 
-            if (args[0].equals("$disconnect"))
-            {
-                manager.closeAudioConnection();
-            }
+                YoutubeAudioSourceManager yotubeASM = new YoutubeAudioSourceManager();
+                //Нужно создать плеер заранне, чтобы при каждом событии он не сбрасывался
+                DiscordAudioPlayer discordAudioPlayer = new DiscordAudioPlayer(guild,
+                        new AudioSourceManager[] {new YoutubeAudioSourceManager()});
 
+                AudioCommandsHandler audioCommandsHandler = new AudioCommandsHandler(discordAudioPlayer);
+
+                audioCommandsHandler.executeCommand(args,voiceChannel,messageChannel);
+
+            }
+            //Распознавание и выполнение аудио команды
+
+            if (event.getMessage().getContentRaw().startsWith("!"))
+            messageChannel.sendMessage(StringOutputCommands.executeCommand(args)).queue();
+            //Распознование и выполнение команды для чата
         }
     }
 }
