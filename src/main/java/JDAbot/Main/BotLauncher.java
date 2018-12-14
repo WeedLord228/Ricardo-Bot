bb6c3514f35e061ad7568221dd4891ddc7a8e4c7
WeedLord228 <zajtzevar@gmail.com>
2018-12-14 22:10:40 +0500

    Updated token
    ligmad8ck0tr0|-| hacker, that wasnt funny

--- src/main/java/JDAbot/Main/BotLauncher.java
+++ src/main/java/JDAbot/Main/BotLauncher.java
@@ -8,7 +8,7 @@ import javax.security.auth.login.LoginException;
 public class BotLauncher {
 
     public static void main(String[] args) throws LoginException {
-        JDA jda = new JDABuilder("NDkzNzA3NDMzMjk5MTQ4ODAw.Do4pGw.eZl9Iu0_GqzJS3kDvNY4KAqzXeU")
+        JDA jda = new JDABuilder("token")
                 .addEventListener(new MyListener())
                 .build();
     }
